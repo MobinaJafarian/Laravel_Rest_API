120d1bde460f8fcf6503d2f75f9237930151d9a5
mobinajafarian <nimobina99@gmail.com>
2023-04-30 16:27:20 +0330

    add UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+}
