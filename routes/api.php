94c4b5e3d6d7e72d7c87fd06b2cfe3141d06f860
mobinajafarian <nimobina99@gmail.com>
2023-05-01 07:57:44 +0330

    add version 1

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::group([], function ($router){
+Route::group(['perfix' => 'v1'], function ($router){
     $router->get('users', [UserController::class, 'index']);
 });
 
