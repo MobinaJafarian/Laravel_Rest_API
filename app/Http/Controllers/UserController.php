4009250a40eb8c3a352a4cee0cdd24f2fc5f26e0
mobinajafarian <nimobina99@gmail.com>
2023-05-01 07:54:50 +0330

    add paginate

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::query()->paginate(5);
         return response()->json($users);
     }
 }
