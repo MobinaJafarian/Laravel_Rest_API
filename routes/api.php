5939d6d3c7257a884482b3d99470fa77aff562fb
mobinajafarian <nimobina99@gmail.com>
2023-05-02 09:27:54 +0330

    add register routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\V1\ArticleController;
+use App\Http\Controllers\V1\UploadController;
 use App\Http\Controllers\V1\UserController;
+use App\Http\Controllers\V2\RegisterController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +21,24 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'v1'], function ($router){
     $router->get('users', [UserController::class, 'index']);
     $router->resource('articles', ArticleController::class);
+
+    $router->get('upload', [UploadController::class, 'index']);
+    $router->post('upload', [UploadController::class, 'store']);
 });
+
+
+Route::group(['prefix' => 'v2'], function ($router) {
+    $router->get('register', [RegisterController::class, 'index']);
+    $router->post('register', [RegisterController::class, 'register']);
+
+    $router->get('login', [LoginController::class, 'login']);
+    $router->post('login', [LoginController::class, 'login']);
+
+    $router->get('email', [HomeController::class, 'email']);
+    $router->post('buy', [HomeController::class, 'buy']);
+});
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
